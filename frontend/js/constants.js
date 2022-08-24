b7de249ef64178541a372e650c7c72358e4af7fe
iwakaru <soy@magentamadness.com>
2022-08-24 20:50:12 +0200

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x5c8660C768c66398c8B40c3B3091080bea433591"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
